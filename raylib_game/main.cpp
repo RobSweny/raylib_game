f036b4a1eab8c24b6ebd039d7bb6cda5b6d2b135
Rob Sweny <Robsweny@gmail.com>
2023-10-01 19:33:39 +0100

    Creating variable for character size, creating a limit to prevent the user from going off the screen

--- raylib_game/main.cpp
+++ raylib_game/main.cpp
@@ -5,33 +5,37 @@
 * CREATE A CHARACTER
 * INPUT: BALL_POSITION (VECT2)
 */
-void CreateCharacter(Vector2 ballPosition)
+void CreateCharacter(Vector2 ballPosition, int charSize)
 {
     // Create Circle
-    DrawCircleV(ballPosition, 50, MAROON);
+    DrawCircleV(ballPosition, charSize, MAROON);
 }
 
 /*
 * CREATING FUNCTION FOR MANAGING THE CONTROLLER
 * INPUT: CHARACTER_POSITION (VECT2)
 */
-Vector2 Controller(Vector2 charPosition)
+Vector2 Controller(Vector2 charPosition, float frameWidth, int charSize)
 {
-    float moveSpeed = 4.0f;
-    if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) charPosition.x += moveSpeed;
-    if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) charPosition.x -= moveSpeed;
+    float moveSpeed = 6.0f;
+    int rightHandBorder = frameWidth - charSize;
+    int leftHandBorder = charSize;
+    if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D))
+        if (charPosition.x < rightHandBorder) charPosition.x += moveSpeed;
+    if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))
+        if (charPosition.x > leftHandBorder) charPosition.x -= moveSpeed;
     return charPosition;
 }
 
 int main() {
     // Determine the Game Window Width and Height
-    const int screenWidth = 800;
-    const int screenHeight = 450;
-    Vector2 charPosition = { (float)screenWidth/2, (float)screenHeight/2 };
-
+    Vector2 screenSize = {800.0f, 450.0f};
+    // Position the character in the middle of the screen
+    Vector2 charPosition = { (float)screenSize.x/2, (float)screenSize.y/2 };
+    int charSize = 50;
 
     // Initialize the Window
-    InitWindow(screenWidth, screenHeight, "My Game");
+    InitWindow(screenSize.x, screenSize.y, "My Game");
 
     // Setting the Frames Per Second
     SetTargetFPS(60);
@@ -44,8 +48,8 @@ int main() {
         // Clear canvas to a specific color to avoid flicker
         ClearBackground(RAYWHITE);
         // Here goes all the Game Logic
-        CreateCharacter(charPosition);
-        charPosition = Controller(charPosition);
+        CreateCharacter(charPosition, charSize);
+        charPosition = Controller(charPosition, screenSize.x, charSize);
 
         // teardown Canvas
         EndDrawing();
