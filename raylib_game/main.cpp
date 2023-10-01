6d66feb86a7a17833773039476883fb0e8ac30e7
Rob Sweny <Robsweny@gmail.com>
2023-10-01 19:00:45 +0100

    Moving controls and character creation to separate functions, this is to remove clutter from main

--- raylib_game/main.cpp
+++ raylib_game/main.cpp
@@ -1,9 +1,34 @@
 #include "raylib.h"
 
+
+/* 
+* CREATE A CHARACTER
+* INPUT: BALL_POSITION (VECT2)
+*/
+void CreateCharacter(Vector2 ballPosition)
+{
+    // Create Circle
+    DrawCircleV(ballPosition, 50, MAROON);
+}
+
+/*
+* CREATING FUNCTION FOR MANAGING THE CONTROLLER
+* INPUT: CHARACTER_POSITION (VECT2)
+*/
+Vector2 Controller(Vector2 charPosition)
+{
+    float moveSpeed = 4.0f;
+    if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) charPosition.x += moveSpeed;
+    if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) charPosition.x -= moveSpeed;
+    return charPosition;
+}
+
 int main() {
-    // Determin the Game Window Width and Height
+    // Determine the Game Window Width and Height
     const int screenWidth = 800;
     const int screenHeight = 450;
+    Vector2 charPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+
 
     // Initialize the Window
     InitWindow(screenWidth, screenHeight, "My Game");
@@ -18,8 +43,9 @@ int main() {
         BeginDrawing();
         // Clear canvas to a specific color to avoid flicker
         ClearBackground(RAYWHITE);
-
         // Here goes all the Game Logic
+        CreateCharacter(charPosition);
+        charPosition = Controller(charPosition);
 
         // teardown Canvas
         EndDrawing();
@@ -27,3 +53,4 @@ int main() {
     CloseWindow();
     return 0;
 }
+
