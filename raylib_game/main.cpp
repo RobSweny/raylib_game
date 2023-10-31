c4905d0b0f95e96a7ac81f159cdeb1beb0323e69
Rob Sweny <Robsweny@gmail.com>
2023-10-31 21:14:30 +0000

    Added projectiles

--- raylib_game/main.cpp
+++ raylib_game/main.cpp
@@ -1,15 +1,26 @@
 #include "raylib.h"
+#include <math.h>
+#include <vector>
 
-// Initialise the camera
-Camera2D camera = { 0 };
+
 // Determine the Game Window Width and Height
-Vector2 screenSize = {1000.0f, 550.0f};
+Vector2 screenSize = {1200.0f, 800.0f};
 // Position the character in the middle of the screen
-Vector2 charPosition = { (float)screenSize.x/2, (float)screenSize.y/2 };
+Vector2 charPosition = { (float) screenSize.x/2, (float) screenSize.y/2 };
 // Set the size of the character
 const int characterSize = 50;
 // Character move speed
-const float moveSpeed = 6.0f;
+float moveSpeed = 4.0f;
+int projectileSpeed = 5;
+typedef struct Projectile
+{
+    Vector2 position;
+    Vector2 direction;
+    float speed;
+} Projectile;
+
+// Array of struct Projectile
+std::vector<Projectile> projectiles;
 
 /* 
 * CREATE A CHARACTER
@@ -21,39 +32,54 @@ void CreateCharacter()
     DrawCircleV(charPosition, characterSize, MAROON);
 }
 
+void Shoot()
+{
+    // Calculate the direction vector from player to mouse when shooting starts
+    Vector2 mousePos = GetMousePosition();
+    Vector2 direction;
+
+    // Calculate the direction vector from player to mouse when shooting starts
+    direction.x = mousePos.x - charPosition.x;
+    direction.y = mousePos.y - charPosition.y;
+    
+    float length = sqrt(direction.x * direction.x + direction.y * direction.y);
+
+    // Normalize the direction vector and scale it by the projectile speed
+    if (length > 0)
+    {
+        float scaleFactor = projectileSpeed / length;
+        direction.x *= scaleFactor;
+        direction.y *= scaleFactor;
+    }
+    
+    Projectile newProjectile = {
+        .position = charPosition,
+        .direction = direction,
+        .speed = 1.0f
+    };
+
+    // Push new projectile to the back of the array
+    projectiles.push_back(newProjectile);
+}
+
+
 /*
 * CREATING FUNCTION FOR MANAGING THE CONTROLLER
 * INPUT: CHARACTER_POSITION (VECT2)
 */
 Vector2 Controller()
 {
-    if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D))
-        if (charPosition.x) charPosition.x += moveSpeed;
-        
-    if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A))
-        if (charPosition.x) charPosition.x -= moveSpeed;
-
-    if (IsKeyDown(KEY_W) || IsKeyDown(KEY_S) || IsKeyDown(KEY_A) || IsKeyDown(KEY_D)) 
-        camera.zoom += 0.005f;
-    else 
-        camera.zoom -= 0.005f;
+    if (IsKeyDown(KEY_LEFT_SHIFT)) moveSpeed = 8.0f;
+    else if (IsKeyReleased(KEY_LEFT_SHIFT)) moveSpeed = 4.0f;
+    if (IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) charPosition.y -= moveSpeed;
+    if (IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) charPosition.x -= moveSpeed;
+    if (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) charPosition.y += moveSpeed;
+    if (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) charPosition.x += moveSpeed;
+    if ((IsMouseButtonPressed(MOUSE_LEFT_BUTTON) || IsKeyDown(KEY_SPACE))) Shoot();
     return charPosition;
 }
 
-/*
-* CREATING FUNCTION TO SET UP CAMERA
-*/
-void CameraSetup()
-{
-    camera.target = charPosition;
-    camera.offset = (Vector2){ screenSize.x/2, screenSize.y/2 };
-    camera.rotation = 0.0f;
-    camera.zoom = 1.0f;
-}
-
 int main() {
-    CameraSetup();
-
     // Initialize the Window
     InitWindow(screenSize.x, screenSize.y, "My Game");
 
@@ -65,21 +91,23 @@ int main() {
 
         // Setup Canvas
         BeginDrawing();
-        // Clear canvas to a specific color to avoid flicker
-        ClearBackground(RAYWHITE);
-        camera.target = charPosition;
-        // Prevent the zoom value to avoid extreme zoom levels
-        if (camera.zoom > 0.8f) camera.zoom = 0.8f;
-        if (camera.zoom < 0.5f) camera.zoom = 0.5f;
-        BeginMode2D(camera);  // Start 2D camera mode
-            // Here goes all the Game Logic
+            // Clear canvas to a specific color to avoid flicker
+            ClearBackground(RAYWHITE);
             CreateCharacter();
             charPosition = Controller();
-        EndMode2D();  // End 2D camera mode
+                
+            for (size_t i = 0; i < projectiles.size(); ++i)
+            {
+                // Move the projectile based on the direction vector
+                projectiles[i].position.x += projectiles[i].direction.x;
+                projectiles[i].position.y += projectiles[i].direction.y;
+                // Draw projectile
+                DrawCircleV(projectiles[i].position, 5, BLUE);
+            }
+                
         // teardown Canvas
         EndDrawing();
     }
     CloseWindow();
     return 0;
 }
-
